a9ca04b0ada2337d17cb4d786c533634fd0fb0e9
bsevcik <43259346+bsevcik@users.noreply.github.com>
2019-02-07 18:38:22 -0800

    Completed Assignment

---
+++ js/script.js
@@ -0,0 +1,126 @@
+// STEP 1
+//    var someMonth;
+//    function theMonth();
+//    var currentMonth;
+//    var summerMonth;
+//    var myLibraryFunction;
+
+// STEP 2
+//    4;
+//    "string literal";
+//    true;
+//    null;
+
+// STEP 3
+//    var x = 4;
+//    var x = "string";
+
+// STEP 4
+//    var strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, boolMayWeContactYou
+
+// STEP 5
+//    var strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, boolMayWeContactYou;
+//    var strFirstName = "Ben";
+//    strFirstName = "Ben";
+//    strFirstName += "jamin";
+
+// STEP 6
+//    var x;
+//    x = 1 + " one";
+//    console.log(x);
+
+// STEP 7
+//    var x, y;
+//    x = true + "string Content";
+//    console.log(x);
+//    y = true + 5;
+//    console.log(y);
+
+// STEP 8
+//    var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I';
+//    someString += "'m not sure about the former." + '"';
+//    window.console.log(someString);
+
+// STEP 9
+//    var x = null;
+//    var y;
+//    console.log(x);
+//    console.log(y);
+
+// STEP 10
+//    console.log(typeof("string"));
+//    console.log(typeof(5));
+//    console.log(typeof(true));
+//    var car = {type:"Fiat", model:"500", color:"white"};
+//    console.log(typeof(car));
+//    var x;
+//    console.log(typeof(x));
+
+// STEP 11
+//    var fullName = "Ben Sevcik";
+//    var classType = "JavaScript";
+//    alert("Hello " + fullName + ", welcome to the " + classType + " class!");
+
+// STEP 12
+//    var name = "Ben Sevcik";
+//    var classType = "JavaScript";
+//    alert("Hello " + name + ", welcome to the " + classType + " class!");
+
+// STEP 13
+//    var name = "Ben Sevcik";
+//    var course = "JavaScript";
+//    alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+//    var name = "Ben Sevcik";
+//    var classType = "JavaScript";
+//    alert("Hello " + name + "," + "\n" + "welcome to the " + classType + " class!");
+
+// STEP 15
+//    var name = prompt("what's you name?");
+//    var classType = prompt("what class are you taking?");
+//    alert("Hello " + name + "," + "\n" + "welcome to the " + classType + " class!");
+
+// STEP 16
+//    var name = prompt("what's you name?");
+//    var classType = prompt("what class are you taking?");
+//    alert("Hello " + name + "," + "\n" + "welcome to the " + classType + " class!");
+
+// STEP 17
+//    var x = 10;
+//    var y = 20;
+//    console.log(x + y);
+
+// STEP 18
+//    var x = 20;
+//    x += 20;
+//    console.log(x);
+// STEP 19
+//    var x = 20;
+//    x *= 5;
+//    console.log(x);
+
+// STEP 20
+//    var x = 20 % 3;
+//    x /= 1;
+//    console.log(x);
+
+// STEP 21
+//    var x = 5;
+//    console.log(Boolean(x == 5));
+
+// STEP 22
+//    var x = 5;
+//    console.log(Boolean(x != 5));
+
+// STEP 23
+//    var widget = new Object;
+//    console.log(typeof(widget));
+
+// STEP 24
+//    var widget = new Object;
+//    console.log(widget instanceof Object);
+
+// STEP 25
+//    var widget = new Object;
+//    console.log(widget instanceof String);
